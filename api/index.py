f750508392c431c2c8b213240c84e6900760bdb8
Vasu Garg <vasugarg7860@gmail.com>
2026-06-12 21:40:07 +0530

    CORS policy

--- api/index.py
+++ api/index.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
 from .models import Invoice
 from .invoice_service import build_invoice_pdf
 
+origins = os.environ.get("CORS_ORIGINS", "http://localhost:5187").split(",")
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
+)
 
 @app.post("/api/invoice/generate")
 def generate_invoice(invoice: Invoice):
